f8caa3df7e6efe9c3680b1fbe3456e075ff33b8c
Marcuse26 <marcos.serranomartinez.alumnos@gmail.com>
2025-09-13 18:39:00 +0200

    4

--- src/App.tsx
+++ src/App.tsx
@@ -554,11 +554,9 @@ const Dashboard = ({ students, attendance, invoices, schedules, config }: { stud
 
     // NUEVA PALETA DE COLORES (R1)
     const distinctColors = [
-        '#FF6384', '#36A2EB', '#FFCE56', '#4BC0C0', '#9966FF', '#FF9F40', 
         '#E6194B', '#3CB44B', '#FFE119', '#4363D8', '#F58231', '#911EB4', 
-        '#46F0F0', '#F032E6', '#BCF60C', '#FABEBE', '#008080', '#E6BEFF', 
-        '#9A6324', '#FFFAC8', '#800000', '#AAFFC3', '#808000', '#FFD8B1', 
-        '#000075', '#A9A9A9', '#FFFFFF', '#000000'
+        '#42D4F4', '#F032E6', '#FABEBE', '#469990', '#DBF876', '#BCF60C',
+        '#FFD8B1', '#000075', '#A9A9A9', '#800000', '#AAFFC3', '#808000', 
     ];
 
 
